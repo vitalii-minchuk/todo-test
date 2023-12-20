b3319a3596bba6377072176cf1e4f8efc1581550
vitalii-minchuk <mcmin4doutsch@gmail.com>
2023-12-20 12:48:58 +0200

    [remove] - remove env variables

--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -2,5 +2,7 @@ import axios from 'axios'
 
 // create axios instance with config for json placeholder api
 export const jsonPlaceholderApi = axios.create({
-  baseURL: import.meta.env.VITE_GLOB_JSON_PLACEHOLDER_API_HOST,
+  // TODO change to env. after testing
+  // baseURL: import.meta.env.VITE_GLOB_JSON_PLACEHOLDER_API_HOST,
+  baseURL: 'https://jsonplaceholder.typicode.com',
 })
